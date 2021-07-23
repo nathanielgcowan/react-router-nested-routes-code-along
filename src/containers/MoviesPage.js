7f172382b37a646ea31249f244996af489c7e540
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-07-23 17:12:00 -0500

    message

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -2,11 +2,16 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
+import MovieShow from '../components/MovieShow';
 
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
+    // Here we replace the `component` prop with the `render` prop so we can pass the 
+    // route information to the `MovieShow` component
+    <Route exact path={match.url} render={() => <h3>Choose a movie from the list above</h3>}/>
+    <Route path={`${match.url}/:movieId`} render={routerProps => <MovieShow {...routerProps} movies={movies} /> }/>
   </div>
 )
 
-export default MoviesPage
+export default MoviesPage 
